5c077a029729ec80fb662658b49fd40620866c5d
daltooneeq <danilbakurov1702@gmail.com>
2024-12-09 20:20:53 +0300

    words

--- Scripts/server.js
+++ Scripts/server.js
@@ -8,6 +8,7 @@ const path = require("path");
 const axios = require('axios');
 
 const database = require("./database");
+const wordList = require('./wordlist')
 
 const app = express();
 const urlencodedParser = express.urlencoded({extended: false});
@@ -30,28 +31,27 @@ async function Translate(word, lang) {
             console.log(`Перевод для слова "${word}" не найден.`);
         }
     } catch (error) {
-        console.error('Произошла ошибка:', error.response ? error.response.data : error.message);
+        console.error('Произошла ошибка Yandex:', error.response ? error.response.data : error.message);
     }
 }
 
-async function randomWord() {
-    try {
-        const res = fetch('https://random-word-api.herokuapp.com/word?number=1')
-        const data = res.json();
-        return data[0]; // Возвращаем рандомное слово
-    } catch (error) {
-        console.error('Ошибка:', error);
-        return null; // Возвращаем null в случае ошибки
-    }
-}
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(_, res){
     page = path.join(__dirname, '../Pages/index.html');
     res.sendFile(page);
 });
-app.get("/words", function(_, res){
-    page = path.join(__dirname, '../Pages/words.html');
-    res.sendFile(page);
+app.get("/words", function(req, res){
+    if (!req.session.login){
+        res.redirect('/auth');
+    }
+    else if (req.session.firstlang == 'none'){
+        res.redirect('/setlang')
+    }
+    else{
+        page = path.join(__dirname, '../Pages/words.html');
+        res.sendFile(page);
+    }
 });
 app.get("/reg", function (_, res){
     page = path.join(__dirname, '../Pages/reg.html');
@@ -62,7 +62,7 @@ app.get("/auth", function (_, res){
     res.sendFile(page);   
 })
 app.get('/setlang', function (_, res){
-    page = path.join(__dirname + '../Pages/setlang.html');
+    page = path.join(__dirname, '../Pages/setlang.html');
     res.sendFile(page);
 })
 
@@ -83,7 +83,7 @@ app.post("/auth", urlencodedParser, async function(req,res){
         req.session.login = auth.login;
         req.session.liked = auth.liked;
         req.session.firstlang = auth.firstlang;
-        req.session.secondland = auth.secondlang;
+        req.session.secondlang = auth.secondlang;
         
         res.send(`Success`);
     }
@@ -93,39 +93,57 @@ app.post('/setlang', urlencodedParser, function (req, res){
     database.setLang(req.body.firstlang, req.body.secondlang, req.session.login);
     req.session.firstlang = req.body.firstlang;
     req.session.secondlang = req.body.secondlang;
+    res.send(`${req.session.firstlang} ${req.session.secondlang}`);
 })
 
 app.post("/words", urlencodedParser, async function(req, res){
-   
-    fs.readFile(path.join(__dirname, '../Pages/words.html'), 'utf8', async (err, data) => {
-        if (err) {
-            console.error(err);
-        }
-        else{
-            const markerEnd = '</table>';
-            try{              
-                const newWord = 'word'
-
-                if (!req.session.words) {
-                    req.session.words = [];
-                }
-                if (!req.session.translations) {
-                    req.session.translations = [];
-                }
-                req.session.words.push(newWord);
-                req.session.translations.push(await Translate(Translate(newWord, `${req.body.firstlang}-en`), `en-${req.body.secondlang}`));
-                
-                const newData = req.session.words.map((word, index) => 
-                    `<tr> <td>${word}</td> <td>${req.session.translations[index]}</td> </tr>`).join('');
-
-                const modifiedData = data.replace(markerEnd, `<div>${newData}</div>\n${markerEnd}`);
-                res.send(modifiedData);
-                
-            } catch (err) {
+    if (!req.session.login){
+        res.redirect('/auth');
+    }
+    else if (req.session.firstlang == 'none'){
+        res.redirect('/setlang')
+    }
+    else{
+        fs.readFile(path.join(__dirname, '../Pages/words.html'), 'utf8', async (err, data) => {
+            if (err) {
                 console.error(err);
             }
-        }
-    });
+            else{
+                const markerEnd = '</table>';
+                try{              
+                    const newWord = wordList.randomWord();
+
+                    if (!req.session.words) {
+                        req.session.words = [];
+                    }
+                    if (!req.session.translations) {
+                        req.session.translations = [];
+                    }
+                    req.session.words.push(newWord);
+
+                    firstTrans = newWord;
+                    if (req.session.firstlang != 'en'){
+                        firstTrans = await Translate(firstTrans, `en-${req.session.firstlang}`)
+                    }
+                    secondTrans = newWord;
+                    if (req.session.secondlang != 'en'){
+                        secondTrans = await Translate(firstTrans, `en-${req.session.secondlang}`)
+                    }
+
+                    req.session.translations.push([firstTrans, secondTrans]);
+
+                    const newData = req.session.words.map((word, index) => 
+                        `<tr> <td><p>${req.session.translations[index][0]}</p></td> <td><p>${req.session.translations[index][1]}</p></td> </tr>`).join('');
+
+                    const modifiedData = data.replace(markerEnd, `<div>${newData}</div>\n${markerEnd}`);
+                    res.send(modifiedData);
+
+                } catch (err) {
+                    console.error(err);
+                }
+            }
+        });
+    }
     
 });
 
